fdd89ac0a82f153d34ef0cfff1eed7c95a883009
Jonathan Hammond <hamman359@gmail.com>
2014-08-26 19:55:07 -0400

    Adding DbContext for project.

---
+++ src/DataAccess/CodeAndCoffeeInfoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAndCoffeeInfo.DataAccess {
+
+	public class CodeAndCoffeeInfoContext : DbContext {
+
+		public CodeAndCoffeeInfoContext() : base("CCI") {
+			
+		}
+	}
+}
