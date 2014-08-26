70697beedfeba4785494d4c5316e548238c150f7
Jonathan Hammond <hamman359@gmail.com>
2014-08-26 19:49:36 -0400

    Adding BaseModel class.

---
+++ src/Core/Model/ModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAndCoffeeInfo.Core.Model {
+
+	public abstract class ModelBase {
+
+		public int Id { get; set; }
+
+		public DateTime CreatedOn { get; set; }
+		public string CreatedBy { get; set; }
+		public DateTime UpdatedOn { get; set; }
+		public string UpdatedBy { get; set; }
+	}
+}
