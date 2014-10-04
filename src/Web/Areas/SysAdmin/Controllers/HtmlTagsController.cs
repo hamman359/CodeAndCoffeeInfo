f43272de7ddf8ae3545d4871ae8778650a27b9c7
Jonathan Hammond <hamman359@gmail.com>
2014-10-03 20:56:59 -0400

    Populating test object for Test Harness

--- src/Web/Areas/SysAdmin/Controllers/HtmlTagsController.cs
+++ src/Web/Areas/SysAdmin/Controllers/HtmlTagsController.cs
@@ -4,18 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CodeAndCoffeeInfo.Web.Areas.SysAdmin.Models;
+using NodaTime;
 
 namespace CodeAndCoffeeInfo.Web.Areas.SysAdmin.Controllers
 {
 	public partial class HtmlTagsController : Controller
     {
 		// GET: SysAdmin/HtmlTags/TagConventions
-		public virtual ActionResult TagConventions()
-		{
-			var model = new TagsTest(); //TODO: Populate this object with test data.
+		public virtual ActionResult TagConventions() {
+
+			LocalDateTime localDateTime = new LocalDateTime(2014, 1, 1, 7, 23, 47, 42);
+
+			var model = new TagsTest {
+				Name = "John Doe",
+				Email = "John.Doe@email.com",
+				Password = "Password1",
+				ConfirmPassword = "Password1",
+				RememberMe = true,
+				Phone = "1234567890",
+				Url = "http:\\google.com",
+				Color = System.Drawing.Color.Green,
+				StartDate = DateTime.Now,
+				LocalDate = new LocalDate(2014, 1, 1),
+				LocalDateTime = localDateTime,
+				OffsetDateTime = new OffsetDateTime(localDateTime, Offset.FromHours(3)),
+				Guid = Guid.NewGuid(),
+				Decimal = 3.14m,
+				Integer = 42,
+				Float = 6.6666f,
+				Double = 3.3333
+			};
 
 			return View(MVC.SysAdmin.HtmlTags.Views.TagConventions, model);
-        }
+		}
 
 		// GET: SysAdmin/HtmlTags/Links
 		public virtual ActionResult Links()
